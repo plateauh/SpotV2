51f55e20af9a350c1e5f6d74b918eb8dc7e93dc8
ThuryaNasser <Thuryaalwehaib@outlook.sa>
2021-04-17 17:31:13 +0300

    DB

--- app/src/main/java/com/example/spotv2/MapActivity.java
+++ app/src/main/java/com/example/spotv2/MapActivity.java
@@ -81,34 +81,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.setMyLocationEnabled(!isGhostMode);
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //those lat and lng MUST be from DB  + userName
         LatLng Thurya = new LatLng(24.686460, 46.840670);
         float color= 210;
         mMap.addMarker(new MarkerOptions()
                 .position(Thurya).icon(BitmapDescriptorFactory.defaultMarker(color))
                 .title("Thurya is here "));
+
+        //those lat and lng MUST be from DB  + userName
         LatLng Fay = new LatLng(24.687966, 46.840708);
         float color5= 180;
         mMap.addMarker(new MarkerOptions()
                 .position(Fay).icon(BitmapDescriptorFactory.defaultMarker(color5))
                 .title("Fay is here "));
 
+        //those lat and lng MUST be from DB + userName
         LatLng Nouf = new LatLng(24.690178, 46.840063);
         float color2= 120;
         mMap.addMarker(new MarkerOptions()
                 .position(Nouf).icon(BitmapDescriptorFactory.defaultMarker(color2))
                 .title("Nouf is here "));
 
+        //those lat and lng MUST be from DB  + userName
         LatLng Latifah = new LatLng(24.690918, 46.841349);
         float color3= 260;
         mMap.addMarker(new MarkerOptions()
                 .position(Latifah).icon(BitmapDescriptorFactory.defaultMarker(color3))
                 .title("Latifah is here "));
 
+        //those lat and lng MUST be from DB + userName
         LatLng Najd = new LatLng(24.6908935, 46.8410352);
         float color4= 240;
         mMap.addMarker(new MarkerOptions()
                 .position(Najd).icon(BitmapDescriptorFactory.defaultMarker(color4))
                 .title("Najd is here "));
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(devLoc));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
         mMap.getUiSettings().setZoomControlsEnabled(true);
